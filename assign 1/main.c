44457994809f9bcf93ace13515f8be9bf31309a4
Yeho Gwon <devstita@gmail.com>
2022-10-29 18:32:09 +0900

    update env. info.

--- assign 1/main.c
+++ assign 1/main.c
@@ -2,7 +2,7 @@
  * Author: 20220358 권예호 (Yeho Gwon)
  * Date: 2022-10-31
  * OS: macOS 13.0
- * IDE Environment: Visual Studio Code 1.72.1
+ * IDE Environment: Visual Studio Code 1.72.2
  * GCC version: gcc 14.0.0
  * Editor and Terminal Font: Consolas
  * Encoding: UTF-8
