5baecc37c0c5a755ae9311571a2741ec1b560bf0
Yeho Gwon <devstita@gmail.com>
2022-10-31 22:33:14 +0900

    added a comment of C standard version

--- assign 1/assn1.c
+++ assign 1/assn1.c
@@ -4,6 +4,7 @@
  * OS: macOS 13.0
  * IDE Environment: Visual Studio Code 1.72.2
  * GCC version: gcc 14.0.0
+ * C standard: C99
  * Editor and Terminal Font: Consolas
  * Encoding: UTF-8
  * End of Line Sequence: LF
