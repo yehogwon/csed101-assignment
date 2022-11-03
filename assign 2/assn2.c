d4f7eee6644dd55330e1952a0a26246c1fe73993
Yeho Gwon <devstita@gmail.com>
2022-11-03 17:28:09 +0900

    developing overall flow

--- assign 2/assn2.c
+++ assign 2/assn2.c
@@ -20,7 +20,7 @@ void set_color_rgb(int r, int g, int b);
 void reset_color();
 
 // Image File Management
-int load_image(const char *filename, int image_rgb[][SIZE][SIZE], float iamge_hsv[][SIZE][SIZE], int *width, int *height);
+int load_image(const char *filename, int image_rgb[][SIZE][SIZE], float image_hsv[][SIZE][SIZE], int *width, int *height);
 void save_image(int image_rgb[][SIZE][SIZE], int width, int height);
 
 // Image Modification
@@ -31,6 +31,33 @@ void print_image(int image_rgb[][SIZE][SIZE], int width, int height);
 void print_histogram(float image_hsv[][SIZE][SIZE], int width, int height);
 
 int main(void) {
+    char file_name[SIZE];
+
+    printf("Enter input filename: ");
+    scanf("%s", file_name);
+
+    int image_rgb[3][SIZE][SIZE], image_hsv[3][SIZE][SIZE];
+    int file_status = load_image(file_name, image_rgb, image_hsv, SIZE, SIZE);
+
+    if (file_status == 0) {
+        printf("File not found: %s \n", file_name);
+        return 404;
+    }
+    
+    printf("======================== \n");
+    printf("  IMAGE COLOR CHANGER  ");
+    printf("======================== \n");
+    printf("  1. Image Histogram \n");
+    printf("  2. Change Color \n");
+    printf("  3. Print Image \n");
+    printf("  4. Save Image \n");
+    printf("  5. Exit \n");
+    printf("Loaded file: %s \n", file_name);
+    printf("\n");
+
+    int menu;
+    printf("Choose menu number >> ");
+    scanf("%d", &menu);
     return 0;
 }
 
