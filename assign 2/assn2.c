dba8b3e7eef4277c9b1d08de40465eff0611990c
Yeho Gwon <devstita@gmail.com>
2022-11-03 17:16:24 +0900

    added a structure for ASSN2

---
+++ assign 2/assn2.c
@@ -0,0 +1,18 @@
+/**
+ * Author: 20220358 권예호 (Yeho Gwon)
+ * Date: 
+ * OS: macOS 13.0
+ * IDE Environment: Visual Studio Code 1.72.2
+ * GCC version: gcc 14.0.0
+ * C standard: C99
+ * Editor and Terminal Font: Consolas
+ * Encoding: UTF-8
+ * End of Line Sequence: LF
+ * Naming Convention: snake_case
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    return 0;
+}
