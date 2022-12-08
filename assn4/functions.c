7d0efc45f7ad7387c4a102f64146913f781be7f2
Yeho Gwon <devstita@gmail.com>
2022-12-08 10:18:56 +0900

    updated doc

--- assn4/functions.c
+++ assn4/functions.c
@@ -3,7 +3,7 @@
  * Author: 20220358 권예호 (Yeho Gwon)
  * Date: 2022.12.03
  * OS: macOS 13.0.1
- * IDE Environment: Visual Studio Code 1.73.1
+ * IDE Environment: Visual Studio Code 1.74.0
  * GCC version: gcc 14.0.0
  * C standard: C99
  * Encoding: UTF-8
